0a68e135b5cd4b22ebfef26c3f7f1e8dc954dc48
Bilikis Orulebaja <bilkisbalogun@gmail.com>
2018-09-11 01:24:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,8 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
+<<<<<<< HEAD
 var conversation = `Let's talk about ${topic}.`;
+=======
+var conversation = `${topic}`;
+>>>>>>> 32270d277068b119588bb137962d977d080f88e9
